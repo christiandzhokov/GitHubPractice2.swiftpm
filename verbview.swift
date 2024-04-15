417ae8f1ac9f09b37f43b2322a56040ab5aaa57b
Tyler <twolff6463@stu.d214.org>
2024-04-15 15:00:08 -0500

    put adejctive view

---
+++ verbview.swift
@@ -0,0 +1,22 @@
+//
+//  verbview.swift
+//  GitHubPractice2
+//
+//  Created by Tyler Wolff on 4/15/24.
+//
+
+import SwiftUI
+
+struct verbView: View {
+    var body: some View {
+        NavigationView{
+            NavigationLink("adjective view"){
+                AdjectiveView()
+            
+            }
+          
+                
+            
+        }
+    }
+}
